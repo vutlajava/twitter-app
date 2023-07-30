00f910bdfb10cf19e929d6e4c04c22fb59b66154
Ashok <119529128+vutlajava@users.noreply.github.com>
2023-07-30 16:09:50 +0200

    Update App.java

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! this is my first update" );
+         System.out.println( "Added extra SOP statement" );
     }
 }
